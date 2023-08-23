4ecc9ee5b8334965923ee3e20cf9128524fc612c
Neil <neilwilliams08@gmail.com>
2023-08-22 20:48:27 -0400

    completed unit test

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -12,5 +12,9 @@ describe("shuffle should...", () => {
     expect(result).toContain(2);
     expect(result).toContain(3);
   }) 
-
+  test("returns an array of the same length",() => {
+    const result = shuffle([1,2,3,4,5]);
+    expect(result.length).toBe(2)
+  })  
 });
+
